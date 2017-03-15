2513788cc78df1dfbb8c833141d9c2ae7a8737f3
angujo <angujomondi@gmail.com>
2017-03-15 08:13:17 +0300

    changes

--- application/views/front/child-page.php
+++ application/views/front/child-page.php
@@ -124,7 +124,7 @@
                         </div>
                         <div class="form-group">
                             <label class="control-label">Picture</label>
-                            <input type="file" name="child_image" accept="image/*">
+                            <input type="file" name="child_image" accept="image/*" required>
                         </div>
                     </div>
                 </div>
